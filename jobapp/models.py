b9e656bf390c11e84a43912f84dc429226f24ad4
avosa <websterb17@gmail.com>
2021-03-20 12:57:08 +0300

    modified models

--- jobapp/models.py
+++ jobapp/models.py
@@ -1,11 +1,9 @@
+from taggit.managers import TaggableManager
+from ckeditor.fields import RichTextField
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 User = get_user_model()
-
-
-from ckeditor.fields import RichTextField
-from taggit.managers import TaggableManager
 
 
 JOB_TYPE = (
@@ -14,34 +12,35 @@ JOB_TYPE = (
     ('3', "Internship"),
 )
 
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
-    
+
 
 class Job(models.Model):
 
-    user = models.ForeignKey(User, related_name='Use', on_delete=models.CASCADE) 
+    user = models.ForeignKey(User, related_name='Use',
+                             on_delete=models.CASCADE)
     title = models.CharField(max_length=300)
     description = RichTextField()
     tags = TaggableManager()
     location = models.CharField(max_length=300)
     job_type = models.CharField(choices=JOB_TYPE, max_length=1)
-    category = models.ForeignKey(Category,related_name='Category', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name='Category', on_delete=models.CASCADE, null=True, blank=True)
     salary = models.CharField(max_length=30, blank=True)
     company_name = models.CharField(max_length=300)
     company_description = RichTextField(blank=True, null=True)
-    url = models.URLField(max_length=200)
-    last_date = models.DateField()
+    url = models.URLField(max_length=200, null=True, blank=True)
+    last_date = models.DateField(null=True, blank=True)
     timestamp = models.DateTimeField(auto_now=True)
-
 
     def __str__(self):
         return self.title
 
- 
 
 class Applicant(models.Model):
 
@@ -49,19 +48,15 @@ class Applicant(models.Model):
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
 
-
     def __str__(self):
         return self.job.title
 
-
-  
 
 class BookmarkJob(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
-
 
     def __str__(self):
         return self.job.title
